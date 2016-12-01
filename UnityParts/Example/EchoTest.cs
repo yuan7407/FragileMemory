7ef4a898b7a83cd01b649ea9e01ee867d034f916
MichaelYuan <yuan7407@gmail.com>
2016-12-01 18:46:57 +0800

    UnityParts
    This is the basic model for receiving data from P5 using unity. Open
    Echo.unity for the test.

---
+++ UnityParts/Example/EchoTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EchoTest : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		WebSocket w = new WebSocket(new Uri("ws://192.168.1.208:9091"));
+		yield return StartCoroutine(w.Connect());
+		//w.SendString("hello world");
+		//int i=0;
+		while (true)
+		{
+			string reply = w.RecvString();
+			if (reply != null)
+			{
+				Debug.Log ("Received: "+reply);
+				//w.SendString("Hi there"+i++);
+				//w.SendString("300");
+			}
+			if (w.error != null)
+			{
+				Debug.LogError ("Error: "+w.error);
+				break;
+			}
+			yield return 0;
+		}
+		w.Close();
+	}
+}
